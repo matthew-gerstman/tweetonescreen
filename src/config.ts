c1c6edd82769bc711617b4a9daaa03dca33f175e
Matthew Gerstman <matthew.gerstman@gmail.com>
2021-02-04 16:37:18 -0500

    Turn off refreshes

--- src/config.ts
+++ src/config.ts
@@ -8,9 +8,10 @@ export const TICK_RATE = 1000 * 10;
 const urlParams = new URLSearchParams(window.location.search);
 
 const urlWidth = urlParams.get("width");
+const width = (urlWidth && parseInt(urlWidth, 10)) || 500;
 export const TWEET_OPTIONS = {
   // https://developer.twitter.com/en/docs/twitter-for-websites/embedded-tweets/guides/embedded-tweet-parameter-reference
-  width: urlWidth || 500,
+  width,
   theme: "dark",
   // align: 'center', // right | center | left
   // conversation, // none
@@ -18,7 +19,7 @@ export const TWEET_OPTIONS = {
 export const BACKGROUND =
   process.env.NODE_ENV !== "production" ? "black" : undefined;
 
-const REFRESH_WINDOW = IS_PROD;
+const REFRESH_WINDOW = urlParams.get("refresh") != null;
 if (REFRESH_WINDOW) {
   setTimeout(() => window.location.reload(), 10000);
 }
