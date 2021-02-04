7202bf86ff7faacbd195c9bcba2ab51868bd50de
Matthew Gerstman <matthew.gerstman@gmail.com>
2021-02-04 09:58:16 -0500

    Change window width add refresh rate

--- src/config.ts
+++ src/config.ts
@@ -7,12 +7,19 @@ export const TICK_RATE = 1000 * 10;
 
 const urlParams = new URLSearchParams(window.location.search);
 
+const urlWidth = urlParams.get("width");
 export const TWEET_OPTIONS = {
   // https://developer.twitter.com/en/docs/twitter-for-websites/embedded-tweets/guides/embedded-tweet-parameter-reference
-  width: urlParams.get("width") || 500,
+  width: 400,
   theme: "dark",
   // align: 'center', // right | center | left
   // conversation, // none
 };
 export const BACKGROUND =
   process.env.NODE_ENV !== "production" ? "black" : undefined;
+
+const REFRESH_WINDOW = IS_PROD;
+
+if (REFRESH_WINDOW) {
+  setTimeout(() => location.reload(), 10000);
+}
